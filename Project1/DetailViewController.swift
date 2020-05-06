37b41e6025d0e9ec8c88ace8ea9cc35e32999c56
Mikhail Strizhenov <ramory.lorch@icloud.com>
2020-05-06 15:29:12 +0300

    added some core graphics

--- Project1/DetailViewController.swift
+++ Project1/DetailViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreGraphics
 
 class DetailViewController: UIViewController {
     @IBOutlet var imageView: UIImageView!
@@ -37,11 +38,39 @@ class DetailViewController: UIViewController {
         navigationController?.hidesBarsOnTap = false
     }
     @objc func shareTapped() {
-        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
+        guard let image = imageView.image else {
             print("No image found")
             return
         }
-        let vc = UIActivityViewController(activityItems: ["\(selectedImage!)", image], applicationActivities: [])
+        let renderer = UIGraphicsImageRenderer(size: imageView.image!.size)
+        let imageToRender = renderer.image { ctx in
+            image.draw(at: .zero)
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .center
+            
+            let attrs: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 48),
+                .paragraphStyle: paragraphStyle
+            ]
+            
+            let string = "From Strom Viewer"
+            
+            let attributedString = NSAttributedString(string: string, attributes: attrs)
+            
+            let rectangle = CGRect(x: 0, y: 0, width: imageView.image!.size.width, height: 100)
+            
+            ctx.cgContext.setFillColor(UIColor.white.cgColor)
+            ctx.cgContext.setStrokeColor(UIColor.black.cgColor)
+            
+            ctx.cgContext.setLineWidth(5)
+            
+            ctx.cgContext.addRect(rectangle)
+            ctx.cgContext.drawPath(using: .fillStroke)
+            
+            attributedString.draw(with: CGRect(x: 0, y: 0, width: imageView.image!.size.width, height: 100), options: .usesLineFragmentOrigin, context: nil)
+            
+        }
+        let vc = UIActivityViewController(activityItems: ["\(selectedImage!)", imageToRender], applicationActivities: [])
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
